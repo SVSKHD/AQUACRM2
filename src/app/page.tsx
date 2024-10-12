3f112759f0848b31d6b876ec4b31cbccb32addab
SVSKHD <8svskhd@gmail.com>
2024-10-12 12:47:10 +0530

    ignore
    ignore description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import {useSelector} from "react-redux"
 
 const AquaHome = () =>{
-const {user} = useSelector((state)=>({...state}))
+const {user} = useSelector((state:any)=>({...state}))
   return(
   <>
   {user.userState?"hello user":"hello no user "}
